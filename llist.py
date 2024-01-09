e4a5858e0b6eb1f844b81393f5d110f9f46ec528
Shajith29 <shajith0829@gmail.com>
2024-01-09 22:34:34 +0530

    reverse of linked list

--- llist.py
+++ llist.py
@@ -103,13 +103,85 @@ class LinkedList(object):
         prev.next = curr_node.next
         curr_node = None
 
+        #function to calculate the length of the list iteratively
 
+    def length_iterative(self):
+        count = 0
+        if self.head is None:
+            count = 0
+        
+        
+        curr_node = self.head
+        while curr_node:
+            curr_node = curr_node.next
+            count += 1
+        
+        return count
+    
+    #function to find the length of the list recursively
+    
+    def length_recursive(self,node):
+        if node is None:
+            return 0
+        
+        return 1 + self.length(node.next)
+
+
+    
+    def swap_nodes(self,key_1,key_2):
+      if key_1 == key_2:
+          return 
+      
+      prev_1 = None
+      curr_1 = self.head
+
+      while curr_1 and curr_1.data != key_1:
+          prev_1 = curr_1
+          curr_1 = curr_1.next
+
+      prev_2 = None
+      curr_2 = self.head
+
+      while curr_2 and curr_2.data != key_2:
+          prev_2 = curr_2
+          curr_2 = curr_2.next
+
+      if not curr_1 or not curr_2:
+          return 
+      
+      if prev_1:
+          prev_1.next = curr_2
+      else:
+          self.head = curr_2
+
+      if prev_2:
+          prev_2.next = curr_1
+      else:
+          self.head = curr_1
+
+      curr_1.next,curr_2.next = curr_2.next,curr_1.next
+
+
+
+    def reverse_iterative(self):
+        prev = None
+        curr_node = self.head
+
+        while curr_node:
+            nxt = curr_node.next
+            curr_node.next = prev
+            prev = curr_node
+            curr_node = nxt
+
+        self.head  = prev
+
+        
 
         
 ll = LinkedList()
 ll.append(1)
 ll.append(2)
 ll.append(3)
-ll.append(5)
-ll.delete_position(3)
+ll.append(4)
+ll.reverse_iterative()
 ll.print()
